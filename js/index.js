63f0bad074617626efbc17098ab79bcd75e2743a
Artem Bannikov <38352426+mopjiex@users.noreply.github.com>
2022-12-02 18:42:53 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const headerMenu = document.querySelector('.header__menu');
 const navigation = document.querySelector('.nav');
 
 mobileMenu.addEventListener('click', () => {
-    if(getComputedStyle(navigation).display == 'none' && navigation.classList.contains('disable')) {
+    if(getComputedStyle(navigation).display == 'none')) {
         navigation.style.display = 'block';
         
     } else {

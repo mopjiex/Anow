073fa496c576ff05fcd32a013176321d70295c45
mopjiex <artem1bannikov@gmail.com>
2022-12-02 23:56:53 +0300

    fixed

--- js/index.js
+++ js/index.js
@@ -1,13 +1,19 @@
 const mobileMenu = document.querySelector('.mobile');
 const navigation = document.querySelector('.nav');
-
+const logo = document.querySelector('.header__logo-link');
+const benefitsCircle = document.querySelectorAll('.benefits__circle');
 mobileMenu.addEventListener('click', () => {
-    if(getComputedStyle(navigation).display == 'none')) {
-        navigation.style.display = 'block';
-        
+    if(getComputedStyle(navigation).display == 'none') {
+        navigation.style.display = 'block'; 
+        logo.style.display = 'none'; 
+        benefitsCircle.forEach(index => {
+            index.style.display = 'none';
+        })
     } else {
         navigation.style.display = 'none';
-       
-
+        logo.style.display = 'block'; 
+        benefitsCircle.forEach(index => {
+            index.style.display = 'block';
+        })
     }
 })
